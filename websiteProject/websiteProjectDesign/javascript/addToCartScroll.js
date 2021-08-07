c16ea5121445c3e245af4a8c3cea56fe617dcabb
CRBeverley <70474913+CRBeverley@users.noreply.github.com>
2021-08-07 15:13:39 -0400

    Updates websiteProject indexMain

---
+++ websiteProject/websiteProjectDesign/javascript/addToCartScroll.js
@@ -0,0 +1,11 @@
+"use strict";
+// const scrollContainer = document.querySelector("addToCartScroll");
+
+// scrollContainer.onclick = function () {
+//   document.querySelector("addToCartScroll").scrollLeft += 20;
+// };
+
+// scrollContainer.addEventListener("wheel", (evt) => {
+//   evt.preventDefault();
+//   scrollContainer.scrollRight += evt.deltaY;
+// });
