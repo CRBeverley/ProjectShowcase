43ec416a92cda13d41b54cac19f2af00efe2481e
CRBeverley <70474913+CRBeverley@users.noreply.github.com>
2021-08-09 16:27:00 -0400

    added animation to login page

---
+++ websiteProject/websiteProjectDesign/javascript/createAccount.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// let displaySetting = document.querySelector(".createAccount").style.display;
+// displaySetting = "flex";
+
+document
+  .querySelector("#createAccountLink")
+  .addEventListener("click", function () {
+    setTimeout(() => {
+      document.querySelector(".login").style.display = "none";
+    }, 200);
+
+    document.querySelector(".createAccount").style.display = "block";
+  });
